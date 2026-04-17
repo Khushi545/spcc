82733aeac6e355434c84dc809b68ed87a52262dd
Khushi <khushikhatri545gmail.com>
2026-04-17 23:51:07 +0530

    lex update

--- lex.c
+++ lex.c
@@ -1,32 +1,22 @@
+%{
 #include <stdio.h>
-#include <ctype.h>
+
+int chars=0, words=0, lines=0, spaces=0, tabs=0, nums=0, sent=0;
+%}
+
+%%
+[0-9]+        { nums++; chars += yyleng; }
+\n            { lines++; chars++; }
+[ ]           { spaces++; chars++; }
+\t            { tabs++; chars++; }
+[.!?]         { sent++; chars++; }
+[a-zA-Z]+     { words++; chars += yyleng; }
+.             { chars++; }
+%%
 
 int main() {
-    char str[200];
-    int chars=0, words=0, lines=0, spaces=0, tabs=0, nums=0, sent=0;
-    int inword=0;
-
-    printf("Enter text:\n");
-    fgets(str, sizeof(str), stdin);
-
-    lines = 1; // since single input line
-
-    for (int i=0; str[i]!='\0'; i++) {
-        chars++;
-
-        if (str[i]==' ') spaces++;
-        if (str[i]=='\t') tabs++;
-        if (str[i]=='.'||str[i]=='!'||str[i]=='?') sent++;
-
-        if (isalnum(str[i])) {
-            if (!inword) { words++; inword=1; }
-        } else inword=0;
-
-        if (isdigit(str[i])) {
-            nums++;
-            while (isdigit(str[i+1])) i++;
-        }
-    }
+    printf("Enter text (Ctrl+D to end):\n");
+    yylex();
 
     printf("\nCharacters: %d", chars);
     printf("\nWords: %d", words);
@@ -38,3 +28,28 @@ int main() {
 
     return 0;
 }
+
+int yywrap() { return 1; }
+
+//Save your LEX code as: filename.l
+
+//How to Compile & Run 
+// sudo apt update
+// sudo apt install flex bison gcc
+
+// flex filename.l
+// gcc lex.yy.c -o output -lfl
+// ./output
+
+// Enter text (Ctrl+D to end):
+// Hello world.
+// This is 2026.
+// ^D
+
+
+
+
+
+
+
+
